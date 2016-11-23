4da5786313e2983d81774bb733da90e0fbd7c6f3
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-23 14:12:58 +0000

    can observe life points

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -8,4 +8,13 @@ RSpec.feature "Battle", :type => :feature do
 
     expect(page).to have_text("Welcome Warrior1 and Warrior2")
   end
+
+
+  scenario "Warrior 1 can see Warrior 2's Hit Points" do
+    visit "/"
+    fill_in "name1", :with => "Warrior1"
+    fill_in "name2", :with => "Warrior2"
+    click_button "Submit"
+    expect(page).to have_text("Warrior2 Hit Points: 100")
+  end
 end
