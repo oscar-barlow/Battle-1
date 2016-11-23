b4d59a8a842872e4f5b22b4d93636f9794f8e3be
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-23 13:03:10 +0000

    players can add names

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  post '/names' do
+    @warrior1 = params[:name1]
+    @warrior2 = params[:name2]
+    erb(:play)
+  end
+
   get '/' do
-    'Hello Battle!'
+    erb(:index)
   end
 
   # start the server if ruby file executed directly
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -2,9 +2,10 @@ RSpec.feature "Battle", :type => :feature do
   scenario "User creates a new battle profile" do
     visit "/"
 
-    fill_in "Name", :with => "Warrior1"
-    click_button "Create Warrior"
+    fill_in "name1", :with => "Warrior1"
+    fill_in "name2", :with => "Warrior2"
+    click_button "Submit"
 
-    expect(page).to have_text("Welcome Warrior1")
+    expect(page).to have_text("Welcome Warrior1 and Warrior2")
   end
 end
