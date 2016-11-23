fee9a5e808b51f09232b63447f87cf610cca800d
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-23 17:45:30 +0000

    Refactored global variables

--- app.rb
+++ app.rb
@@ -9,30 +9,28 @@ class Battle < Sinatra::Base
   post '/names' do
     $player_1 = Player.new(params[:name1])
     $player_2 = Player.new(params[:name2])
-    session[:health1] = 100
-    session[:health2] = 100
     redirect '/play'
   end
 
   get '/play' do
-    @warrior1 = $player_1.name
-    @warrior2 = $player_2.name
-    @warrior1_health = $player_1.health
-    @warrior2_health = $player_2.health
+    @warrior1 = $player_1
+    @warrior2 = $player_2
+    #@warrior1_health = $player_1.health
+    #@warrior2_health = $player_2.health
     erb(:play)
   end
 
   get '/attack2' do
-    $player_1.attack($player_2)
-    @attacker = $player_1.name
-    @defender = $player_2.name
+    @attacker = $player_1
+    @defender = $player_2
+    @attacker.attack(@defender)
     erb(:confirmation)
   end
 
   get '/attack1' do
-    $player_2.attack($player_1)
-    @defender = $player_1.name
-    @attacker = $player_2.name
+    @attacker = $player_2
+    @defender = $player_1
+    @attacker.attack(@defender)
     erb(:confirmation)
   end
 
