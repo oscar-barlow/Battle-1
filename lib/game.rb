38cd2b67a39617fd05ab6ed5ac62c84c25b6732c
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-24 11:35:14 +0000

    game allows players to deal damage to each other

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,8 @@ class Game
     @players = [player1, player2]
   end
 
+  def attack(player)
+    player.receive_damage
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,13 +5,22 @@ require 'player'
 
 describe Game do
 
-  let(:player1) { double :player1}
-  let(:player2) { double :player2}
+  let(:player1) { spy :player1}
+  let(:player2) { spy :player2}
   subject(:game) {described_class.new(player1, player2)}
 
   it "should have players in upon initialization" do
     expect(game.players).not_to be_empty
   end
 
+  it "should allow player1 to attack player2" do
+    game.attack(player2)
+    expect(player2).to have_received(:receive_damage)
+  end
+
+  it "should allow player 2 to attack player 1" do
+    game.attack(player1)
+    expect(player1).to have_received(:receive_damage)
+  end
 
 end
