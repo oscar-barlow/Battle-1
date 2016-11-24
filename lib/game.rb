98458fb843c4977a3cf94062fd9a7ff61a9f0f34
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-24 12:03:02 +0000

    refactor game class to store players in instance variables

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 class Game
 
-  attr_reader :players
+  attr_reader :player1, :player2
 
   def initialize(player1, player2)
-    @players = [player1, player2]
+    @player1 = player1
+    @player2 = player2
   end
 
   def attack(player)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,18 @@
 require 'game'
 require 'player'
 
-
-
 describe Game do
 
-  let(:player1) { spy :player1}
-  let(:player2) { spy :player2}
+  let(:player1) { spy :player1 }
+  let(:player2) { spy :player2 }
   subject(:game) {described_class.new(player1, player2)}
 
-  it "should have players in upon initialization" do
-    expect(game.players).not_to be_empty
+  it "should have a player in player 1 slot upon initialization" do
+    expect(game.player1).not_to be_nil
+  end
+
+  it "should have a player in player 2 slot upon initialization" do
+    expect(game.player2).not_to be_nil
   end
 
   it "should allow player1 to attack player2" do
