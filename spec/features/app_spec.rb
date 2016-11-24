6011882935913d9f9367ff65fb04e58f86a2ad77
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-24 11:00:46 +0000

    refactor links to buttons for attacking players

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -11,27 +11,27 @@ RSpec.feature "Battle", :type => :feature do
 
   scenario "Warrior 1 can attack warrior 2" do
     sign_in_and_play
-    click_link("Attack Warrior2")
-    click_link("Wanna fight more?")
+    click_button("Attack Warrior2")
+    click_button("Wanna fight more?")
     expect(page).to have_text("Warrior2 health: 90")
   end
 
   scenario "Warrior 2 can attack warrior 1" do
     sign_in_and_play
-    click_link("Attack Warrior1")
-    click_link("Wanna fight more?")
+    click_button("Attack Warrior1")
+    click_button("Wanna fight more?")
     expect(page).to have_text("Warrior1 health: 90")
   end
 
   scenario "Return a confirmation of Warrior1 attacking Warrior2" do
     sign_in_and_play
-    click_link("Attack Warrior2")
+    click_button("Attack Warrior2")
     expect(page).to have_text("Warrior1 attacked Warrior2")
   end
 
   scenario "Return a confirmation of Warrior2 attacking Warrior1" do
     sign_in_and_play
-    click_link("Attack Warrior1")
+    click_button("Attack Warrior1")
     expect(page).to have_text("Warrior2 attacked Warrior1")
   end
 end
