e05da69ee408270bf381d97d864c942519dac01d
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-24 12:07:01 +0000

    refactor routes to keep players in game global variable

--- app.rb
+++ app.rb
@@ -8,29 +8,29 @@ class Battle < Sinatra::Base
 
 
   post '/names' do
-    $player_1 = Player.new(params[:name1])
-    $player_2 = Player.new(params[:name2])
-    $game = Game.new($player_1, $player_2)
+    player_1 = Player.new(params[:name1])
+    player_2 = Player.new(params[:name2])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @warrior1 = $player_1
-    @warrior2 = $player_2
+    @warrior1 = $game.player1
+    @warrior2 = $game.player2
     erb(:play)
   end
 
   get '/attack2' do
-    @attacker = $player_1
-    @defender = $player_2
-    $game.attack($player_2)
+    @attacker = $game.player1
+    @defender = $game.player2
+    $game.attack($game.player2)
     erb(:confirmation)
   end
 
   get '/attack1' do
-    @attacker = $player_2
-    @defender = $player_1
-    $game.attack($player_1)
+    @attacker = $game.player2
+    @defender = $game.player1
+    $game.attack($game.player1)
     erb(:confirmation)
   end
 
