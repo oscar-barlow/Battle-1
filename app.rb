9fe38d798ae0c49ba65cfbaf664bfbc8a76953e1
Chris <chrixs99@hotmail.com>
2016-11-25 15:10:44 +0000

    refactored global variables out of controller

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class Battle < Sinatra::Base
   enable :sessions
   configure(:development) { set :session_secret, "secret" }
 
+  before do
+    @game = Game.instance
+  end
+
 
   get '/' do
     erb(:index)
@@ -14,30 +18,30 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:name1])
     player_2 = Player.new(params[:name2])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(Game, player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @warrior1 = $game.player1
-    @warrior2 = $game.player2
-    @active_player = $game.active_player
-    @inactive_player = $game.inactive_player
+    @warrior1 = @game.player1
+    @warrior2 = @game.player2
+    @active_player = @game.active_player
+    @inactive_player = @game.inactive_player
     erb(:play)
   end
 
   get '/attack' do
-    @attacker = $game.active_player
-    @defender = $game.inactive_player
-    $game.attack($game.inactive_player)
+    @attacker = @game.active_player
+    @defender = @game.inactive_player
+    @game.attack(@game.inactive_player)
     erb(:confirmation)
   end
 
   get '/end_turn' do
-    if $game.is_battle_over?
+    if @game.is_battle_over?
       redirect '/lose'
     else
-      $game.switch_active_player
+      @game.switch_active_player
       redirect '/play'
     end
   end
